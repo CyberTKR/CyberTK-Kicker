a235ffabf190d3e36194609e02ca2659b8135a3f
TKR <cybertk7@gmail.com>
2018-07-03 16:37:00 +0000

    Upload New File

---
+++ src/kicker.js
@@ -0,0 +1,210 @@
+const LineAPI = require('./api');
+const request = require('request');
+const fs = require('fs');
+const unirest = require('unirest');
+const webp = require('webp-converter');
+const path = require('path');
+const rp = require('request-promise');
+const config = require('./config');
+const { Message, OpType, Location } = require('../curve-thrift/line_types');
+
+const myBot = [''];
+var groupList = new Array();
+var vx = {};var midnornama,pesane,kickhim;var waitMsg = "no";
+var komenTL = "➧Hello \n     ➧I'm Kicker My Creator ⇩\n\nhttp://line.me/ti/p/~cybertk0\n\nCyͥbeͣrͫ TK\n   Kicker & Security  V.2.1.0    !"; 
+var limitposts = '10'; 
+
+/*    
+──────────────────────────────▓▓█───────
+────────────────────────────▒██▒▒█──────
+───────────────────────────█▓▓▓░▒▓▓─────
+─────────────────────────▒█▓▒█░▒▒▒█─────
+────────────────────────▒█▒▒▒█▒▒▒▒▓▒────
+─▓▓▒░──────────────────▓█▒▒▒▓██▓▒░▒█────
+─█▓▓██▓░──────────────▓█▒▒▒▒████▒▒▒█────
+─▓█▓▒▒▓██▓░──────────▒█▒▒▒▒▒██▓█▓░░▓▒───
+─▓▒▓▒▒▒▒▒▓█▓░──░▒▒▓▓██▒▒▒▒▒▒█████▒▒▒▓───
+─▓░█▒▒▒▒▒▒▒▓▓█▓█▓▓▓▓▒▒▒▒▒▒▒▒██▓██▒░▒█───
+─▓░▓█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓████▒▒▒█───
+─▓░▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▓██░░░█───
+─▓░▓███▒▒▒▒▒▒▒▒▒▒▒▓█▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▒▓▓──
+─▒▒▒██▓▒▓█▓▒▒▒▒▒▒▒▓▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒▒▓▒█──
+──▓▒█▓▒▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▓█▓▓▓▓█▓▒▒▒▒▒▒▒▓▒─
+──▓▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓──────▓█▓▒▒▒▒▒▓█─
+──▒▒▓▒▒▒▓▓▓▒▒▒▒▒▒▒▒▒▓▓───░▓▓───█▓▒▒▒▒▒█─
+───█▒▒▓▓▓▒▒▓▓▒▒▒▒▒▒▓▓───█████▓──█▓▒▒▒▒▓▒
+───▓▓█▒─────▒▓▒▒▒▒▒█───░██████──░█▒▒▒▒▓▓
+───▓█▒──▒███─▒▓▒▒▒▒█────██████───▓▒▒▒▒▒▓
+───██───█████─█▒▒▒▒█─────███▓────▓▓▒▒▒▒▓
+───█▓───█████─▒▓▒▒▒█─────────────█▓▓▓▒▒▓
+───█▓───░███──░▓▒▒▒▓█──────────░█▓▒▒▒▓▒▓
+───██─────────▒▓▒▒▒▒▓▓──────░▒▓█▓────░▓▓
+───▓█░────────█▓██▓▒▒▓█▓▓▓▓██▓▓▒▓▒░░▒▓▒▓
+───▒██░──────▓▒███▓▒▒▒▒▓▓▓▓▒▒▒▒▒▒▓▓▓▓▒▓─
+────█▓█▓▓▒▒▓█▓▒░██▒▒▓▓█▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓█▒
+────▓─░▓▓▓▓▓▒▓▓▓▓▒▓▓▓▒▓▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓
+────▒▒▒▓▒▒▒▒▒▒▓█░─░░░─▓▓▒▒▒▒▒▒▒▒▒▒▒▓██▓▒
+─────█▓▒▒▒▒▒▒▒▒▓▓─░░░─▓▓▒▒▒▒▒▒▒▒▒▓▓▓▒▒▓▒
+──────██▓▓▒▒▒▒▒▒█▒░░░░█▒▒▒▒▒▒▒▒▓█▓▓▒▒▒▒▒
+─────░─▒██▓▓▒▒▒▒▒█▓▒▒▓▒▒▒▒▒▒▓███▓▒▒▒▒▒▓▓
+──────────░▒▓▓▓▓▒▒▓▓▓▓▓▓████▓▓█▒▒▒▒▒▓▓█░*/
+
+function isTkkicKerBot(param) {
+    return myBot.includes(param);
+}
+
+function isBanned(banList, param) {
+    return banList.includes(param);
+}
+
+function firstToUpperfCase(str) {
+    return str.substr(0, 1).toUpperCase() + str.substr(1);
+}
+
+function isTGet(string,param){
+	return string.includes(param);
+}
+
+function isImg(param) {
+    return imgArr.includes(param);
+}
+
+function ambilKata(params, kata1, kata2){
+    if(params.indexOf(kata1) === false) return false;
+    if(params.indexOf(kata2) === false) return false;
+    let start = params.indexOf(kata1) + kata1.length;
+    let end = params.indexOf(kata2, start);
+    let returns = params.substr(start, end - start);
+    return returns;
+}
+
+class LINE extends LineAPI {
+    constructor() {
+        super();
+		this.limitposts = limitposts;
+        this.receiverID = '';
+        this.checkReader = [];
+        this.stateStatus = {
+            kicker: 1,
+			merhaba: 1 
+        }
+    }
+  
+/*
+███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█ 
+████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█ 
+███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█ 
+████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█ 
+███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
+█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
+█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
+████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
+████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██ 
+█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██ 
+█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███ 
+██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███ 
+███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████ 
+███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████ 
+████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████ 
+█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████ 
+██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████ 
+███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████ 
+██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████ 
+███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████*/
+
+    poll(operation) {
+        if(operation.type == 25 || operation.type == 26) {
+            const txt = (operation.message.text !== '' && operation.message.text != null ) ? operation.message.text : '' ;
+            let message = new Message(operation.message);
+            this.receiverID = message.to = (operation.message.to === myBot[0]) ? operation.message.from_ : operation.message.to ;
+            Object.assign(message,{ ct: operation.createdTime.toString() });
+            if(waitMsg == "yes" && operation.message.from_ == vx[0] && this.stateStatus.mute != 1){
+				this.textMessage(txt,message,message.text)
+			}else if(this.stateStatus.mute != 1){this.textMessage(txt,message);
+			}else if(txt == "unmute" && isTkkicKerBot(operation.message.from_) && this.stateStatus.mute == 1){
+			    this.stateStatus.mute = 0;
+			    this._sendMessage(message,"ヽ(^。^)ノ")
+		    }else{console.info("muted");}
+        }
+				
+		if(operation.type == 16 && this.stateStatus.merhaba == 1){
+			let merhaba = new Message();
+			merhaba.to = operation.param1;
+			merhaba.text = "➧Hello Friends\n     ➧I'm Kicker My Creator ⇩\n\nhttp://line.me/ti/p/~cybertk0\n\nCyͥbeͣrͫ TK\n   Kicker & Security  V.2.1.0    !";
+			this._client.sendMessage(0, merhaba);
+		}
+
+		if(operation.type == 5 && this.stateStatus.merhaba == 1) {
+            let ekleme = new Message();
+			ekleme.to = operation.param1;
+			ekleme.text = "Creator: line://ti/p/~cybertk0 (~CyberTK~)";
+			this._client.sendMessage(0, ekleme);
+        }
+    }
+	
+	async aLike(){
+		if(config.chanToken && config.doing == "no"){
+			config.doing = "ya";
+		    this._autoLike(config.chanToken,limitposts,komenTL);
+		}
+	}
+
+    async searchGroup(gid) {
+        let listPendingInvite = [];
+        let thisgroup = await this._getGroups([gid]);
+        if(thisgroup[0].invitee !== null) {
+            listPendingInvite = thisgroup[0].invitee.map((key) => {
+                return key.mid;
+            });
+        }
+        let listMember = thisgroup[0].members.map((key) => {
+            return { mid: key.mid, dn: key.displayName };
+        });
+
+        return { 
+            listMember,
+            listPendingInvite
+        }
+    }
+
+	
+	async searchRoom(rid) {
+        let thisroom = await this._getRoom(rid);
+        let listMemberr = thisroom.contacts.map((key) => {
+            return { mid: key.mid, dn: key.displayName };
+        });
+
+        return { 
+            listMemberr
+        }
+    }
+
+
+    async textMessage(textMessages, seq, param, lockt) {
+        const [ cmd, payload ] = textMessages.split(' ');
+		const gTicket = textMessages.split('line://ti/g/');
+		const linktxt = textMessages.split('http');
+        const txt = textMessages.toLowerCase();
+        const messageID = seq.id;
+		const cot = txt.split('@');
+		const com = txt.split(':');
+		const cox = txt.split(' ');
+		
+      if(txt === 'cybertk' && this.stateStatus.kicker == 1 && seq.toType == 2) {
+          let { listMember } = await this.searchGroup(seq.to);
+          for (var i = 0; i < listMember.length; i++) {
+              if((listMember[i].mid)){
+                  this._kickMember(seq.to,[listMember[i].mid])
+                }
+            }
+				}
+    }
+
+}
+
+module.exports = new LINE();
