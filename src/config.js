805e181eb6452aa1f4bc41c3330c1fadd75d4fac
TKR <cybertk7@gmail.com>
2018-07-04 02:14:35 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ const config = {
   LINE_DOMAIN_2ND: 'gd2.line.naver.jp',
   LINE_DOMAIN_3RD: 'gfs.line.naver.jp', 
   LINE_DOMAIN_4TH: 'gfps.line.naver.jp',
-  LINE_KEEP_X: '/r/keep/p/', //https://obs-sg.line-apps.com/r/keep/p/1508751723510tffffffff
-  LINE_KEEP: '/kp/api/v21/keep', //Keep -> gfs.line.naver.jp/kp/api/v21/keep/sync.json?revision=0&limit=50&serviceType=1 || fetch.json?startRevision=0&limit=50&serviceType=1
+  LINE_KEEP_X: '/r/keep/p/', 
+  LINE_KEEP: '/kp/api/v21/keep',
   LINE_OS_URL: 'os.line.naver.jp',
   LINE_HTTP_URL: '/api/v4/TalkService.do',
   LINE_HTTP_URL_2ND: '/api/v3/TalkService.do',
@@ -20,6 +20,20 @@ const config = {
   LINE_API_QUERY_PATH_SEC: '/F4',
   LINE_POLL_QUERY_PATH_SEC: '/E4',
   LINE_POLL_QUERY_PATH_THI: '/H4',
+ 
+                                                  /*  
+                                                ░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░
+                                                ░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+                                                ░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+                                                ░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+                                                ░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+                                                ██████▄███▄████░░███▄░░░░░░░
+                                                ▓▓▓▓▓▓█░░░█░░░█░░█░░░███░░░░
+                                                ▓▓▓▓▓▓█░░░█░░░█░░█░░░█░░█░░░
+                                                ▓▓▓▓▓▓█░░░░░░░░░░░░░░█░░█░░░
+                                                ▓▓▓▓▓▓█░░░░░░░░░░░░░░░░█░░░░
+                                                ▓▓▓▓▓▓█░░░░░░░░░░░░░░██░░░░░
+                                                ▓▓▓▓▓▓█████░░░░░░░░░██░░░░░░*/
   LINE_NORMAL_POLL_QUERY_PATH: '/NP4',
   LINE_COMPACT_MESSAGE_QUERY_PATH: '/C5',
   LINE_CALL_QUERY_PATH: '/V4',
@@ -29,7 +43,7 @@ const config = {
   LINE_COMMAND_PATH_2: '/S3',
   LINE_CERTIFICATE_URL: '/Q',
   LINE_SQUARE_PATH: '/SQS1',
-  LINE_CPF: '/CPF', //What is this ?
+  LINE_CPF: '/CPF', 
   LINE_CHANNEL_PATH: '/CH4',
   LINE_SHOP_PATH: '/SHOP4',
   LINE_SESSION_LINE_URL: '/authct/v1/keys/line',
@@ -37,8 +51,7 @@ const config = {
   LINE_POST_CONTENT_URL: 'https://os.line.naver.jp/talk/m/upload.nhn',
   LINE_POST_CONTENT_URL_2ND: 'https://obs-sg.line-apps.com/talk/m/upload.nhn',
   X_LINE_APP: 'IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0',
-  //X_LINE_APP: 'CHROMEOS	1.4.13	Chrome_OS	1',
-  ip: '127.0.0.1',
+    ip: '127.0.0.1',
   version: '0.0.2',
   revision: 0,
   hostname: hostname(),
